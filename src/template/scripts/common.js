53e9426c63378c300f72d3ce748fc359ea2b5835
e5ash <io@e5ash.com>
2017-12-30 06:41:08 +0300

    100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,14 @@ $(document).ready(function($) {
 
 	$('.text_phone').mask('+7 (999) 999-99-99');
 	
+	var mobBtn = $('.mobile-btn'),
+			nav = $('.panel__nav'),
+			toggleStr = '_toggle'
+
+	mobBtn.click(function(event) {
+		$(this).toggleClass('mobile-btn' + toggleStr);
+		nav.toggleClass('panel__nav' + toggleStr);
+	});
+	
+
 });
